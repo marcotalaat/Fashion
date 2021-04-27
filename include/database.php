a6ad0b7f774d0ee7c578fdabd81967f3e6d21af8
Marco <64090277+marcotalaat1412@users.noreply.github.com>
2021-04-27 19:33:44 +0200

    Update database.php

--- include/database.php
+++ include/database.php
@@ -1,18 +1,4 @@
 <?php
-/*    $dsn = "mysql:host=sql307.epizy.com;dbname=epiz_28391963_fashion";
-    $user = "epiz_28391963";
-    $pass = "TPDnl4Cp35zk";
-    $option = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-    );
-    
-    try {
-        $db = new PDO($dsn,$user,$pass,$option);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-        echo "Falied " . $e->getMessage();
-    }  */
    $dsn = "mysql:host=localhost;dbname=fashion";
     $user = "root";
     $pass = "";
